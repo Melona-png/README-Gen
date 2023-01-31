8eb609662c153f109e4ed52f5f64352e05298344
Melona-Png <madisont.woodward@gmail.com>
2023-01-31 18:53:27 -0500

    connected utils/template, ran first test of sending answer to README.md, created a couple questions

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# README-Gen
+# test
+
--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-const inquire = require("inquirer");
+// packages
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const template = require("./utils/template");
 
-const require = inquirer
+// questions for prompt
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project',
+        name: 'title',
+      },
+    {
+        type: 'input',
+        message: 'What is the description of this project',
+        name: 'description',
+      },
+    {
+        type: 'list',
+        message: 'What is the project license under',
+        name: 'license',
+        choices: ["MIT","GPG","MOZILLA","NONE"],
+      },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(),fileName),data)
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(function(answers){
+        writeToFile("README.md", template({ ...answers }))
+    })
+}
+
+// Function call to initialize app
+init();
